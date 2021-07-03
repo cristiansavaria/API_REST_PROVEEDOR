39b4e85d201ea3e7db60003864ec3dbf27030479
cristiansavaria <cristian2012sa@gmail.com>
2021-07-03 19:27:35 -0400

    r

--- core/static/core/js/proveedor.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-   $.get("localhost:8000/api/proveedor", data,
-       function (data, textStatus, jqXHR) {
-        console.log(respuesta) 
-       },
-       "json"
-   );
-});
-
